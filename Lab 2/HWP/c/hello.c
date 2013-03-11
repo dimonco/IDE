38a466cf64bf3f43fbec79af69817aa4c6a9a569
Cotet Dumitru <dima.cotet@gmail.com>
2013-03-11 09:02:37 +0200

    Script v0.2

--- Lab 2/HWP/c/hello.c
+++ Lab 2/HWP/c/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main()
+in main()
 {
   printf("Hello world\n");
   return 0;
--- Lab 2/HWP/script.sh
+++ Lab 2/HWP/script.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 if gcc c/hello.c -o c/c_app 2>> report.txt;
-	then #git commit -m 'C Success';
+	then echo "C"; #git commit -a -m 'C Success';
 fi
 if g++ -o cpp/output cpp/hello.cpp 2>> report.txt;
-	then #git commit -m 'C++ Success';
+	then echo "C++"; #git commit -a -m 'C++ Success';
 fi
 if javac java/hello.java 2>> report.txt;
-	then #git commit -m 'Java Success';
+	then echo "Java"; #git commit -a -m 'Java Success';
 fi
 echo '===Output of the results==='
 c/c_app
 cpp/output
 java -cp java hello
 if python python/hello.py 2>> report.txt;
-	then #git commit -m 'Python Success';
+	then echo "Python"; #git commit -a -m 'Python Success';
 fi
 if ruby ruby/hello.rb 2>> report.txt;
-	then #git commit -m 'Ruby Success';
+	then echo "Ruby"; #git commit -a -m 'Ruby Success';
 fi
-cat report.txt | msmtp --host=smtp.mail.ru --port=465 --protocol=smtp --auth=on --user=dimon_co@mail.ru --tsl==on --tsl-trust-file=/etc/ssl/certs/ca-certificates.crt -f dimon_co@mail.ru -t dimon_co@mail.ru
+cat report.txt | msmtp --host=smtp.mail.ru --port=465 --protocol=smtp --auth=on --user=dimon_co@mail.ru -f dimon_co@mail.ru -t dimon_co@mail.ru
 rm report.txt
